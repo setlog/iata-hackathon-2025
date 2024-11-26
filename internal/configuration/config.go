05d312ff5e4809bedc167b84818425d33c123cf7
dkoerner <d.koerner@setlog.com>
2024-11-26 13:33:52 +0100

    remove Temp Log

--- internal/configuration/config.go
+++ internal/configuration/config.go
@@ -1,12 +1,7 @@
 package configuration
 
 import (
-	"fmt"
 	"github.com/spf13/viper"
-	"log"
-	"log/slog"
-	"os"
-	"path/filepath"
 )
 
 type Config struct {
@@ -19,25 +14,22 @@ type Config struct {
 }
 
 func NewConfig() (error, *Config) {
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(dir)
 	env := Config{}
 	v := viper.New()
 	v.AutomaticEnv()
 	if len(v.AllKeys()) == 0 {
 		v.SetConfigType("env")
 		v.SetConfigFile(".env")
+		//docker
 		v.AddConfigPath("/app")
+		//local
+		v.AddConfigPath(".")
 		err := v.ReadInConfig()
 		if err != nil {
 			return err, nil
 		}
 	}
-	slog.Info(v.ConfigFileUsed())
-	err = v.Unmarshal(&env)
+	err := v.Unmarshal(&env)
 	if err != nil {
 		return err, nil
 	}
