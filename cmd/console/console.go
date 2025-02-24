466fd6f92e766f010057486bfcf9d0263fd9aabd
Maurice Borzym <mborzym@setlog.lan>
2025-02-24 23:44:22 +0100

    add statistics

--- cmd/console/console.go
+++ cmd/console/console.go
@@ -5,8 +5,12 @@ import (
 	"com.setlog/internal/model"
 	"com.setlog/internal/service"
 	"encoding/json"
+	"fmt"
+	//"github.com/spf13/viper"
+	"io/fs"
 	"log/slog"
 	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -19,22 +23,85 @@ func main() {
 	token := service.NewTokenService(conf)
 	iata := service.NewIataService(conf, token)
 	hw := service.NewHwbService(conf)
-	payload, err := os.ReadFile("./example/ai_reponse.json")
+	statistic := model.Statistic{}
+	var payloads []FilePayload
+	err = filepath.Walk("../example/", func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		payload, err := os.ReadFile(path)
+		payloads = append(payloads, FilePayload{Payload: payload})
+		return err
+	})
+	//payload, err := os.ReadFile("../example/ai_reponse.json")
 	if err != nil {
 		return
 	}
-	var resp *model.HwbReportResponseVertexAi
-	err = json.Unmarshal(payload, &resp)
-	if err != nil {
-		return
+	for _, payload := range payloads {
+		var resp *model.HwbReportResponseVertexAi
+		err = json.Unmarshal(payload.Payload, &resp)
+		if err != nil {
+			return
+		}
+
+		if isPayloadValid(&statistic, resp) {
+			conv := hw.ConvertResponse(resp)
+
+			err = iata.CreateIataData(conv)
+			if err != nil {
+				slog.Error(err.Error())
+				return
+			}
+		}
+
+	}
+	fmt.Printf("%#v", statistic)
+
+}
+
+func isPayloadValid(statistic *model.Statistic, resp *model.HwbReportResponseVertexAi) bool {
+	pieceFailed := false
+	grossWeightMissing := false
+
+	statistic.TotalFiles++
+	for _, piece := range resp.ShipmentOfPieces {
+		itemNumberMissing := false
+		quantityMissing := false
+		manufacturerMissing := false
+		statistic.TotalNumberOfItems++
+		if piece.ItemNumber == "" {
+			statistic.ItemNumberMissing++
+			itemNumberMissing = true
+		}
+		if piece.Quantity == 0 {
+			statistic.QuantityMissing++
+			quantityMissing = true
+		}
+		if piece.Manufacturer == "" {
+			statistic.ManufacturerMissing++
+			manufacturerMissing = true
+		}
+		if itemNumberMissing || quantityMissing || manufacturerMissing {
+			statistic.NumberOfFailedItems++
+			pieceFailed = true
+		}
 	}
 
-	conv := hw.ConvertResponse(resp)
-
-	err = iata.CreateIataData(conv)
-	if err != nil {
-		slog.Error(err.Error())
-		return
+	if resp.TotalGrossWeight == "" {
+		grossWeightMissing = true
 	}
 
+	if pieceFailed || grossWeightMissing {
+		statistic.FailedFiles++
+		return false
+	}
+	return true
+}
+
+type FilePayload struct {
+	Payload []byte
 }
---
+++ internal/model/statistic.go
@@ -0,0 +1,12 @@
+package model
+
+type Statistic struct {
+	TotalFiles          int
+	FailedFiles         int
+	ItemNumberMissing   int
+	QuantityMissing     int
+	ManufacturerMissing int
+	GrossWeightMissing  int
+	NumberOfFailedItems int
+	TotalNumberOfItems  int
+}
