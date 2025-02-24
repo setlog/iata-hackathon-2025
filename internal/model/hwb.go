4304742c372e4d2547ea4c8fb963ac2cd613375e
Maurice Borzym <mborzym@setlog.lan>
2025-02-24 14:30:27 +0100

    add model for ai site input

--- internal/model/hwb.go
+++ internal/model/hwb.go
@@ -1,4 +1,39 @@
 package model
 
 type HwbReportResponseVertexAi struct {
+	Hawb                 string          `json:"hawb"`
+	Issued               string          `json:"issued"`
+	Pol                  string          `json:"pol"`
+	Poa                  string          `json:"poa"`
+	Etd                  string          `json:"etd"`
+	Eta                  string          `json:"eta"`
+	Flightno             string          `json:"flightno"`
+	CarrierName          string          `json:"carrierName"`
+	CarrierAdress        string          `json:"carrierAdress"`
+	ShipperName          string          `json:"shipperName"`
+	ShippperAdress       string          `json:"shippperAdress"`
+	ConsigneeName        string          `json:"consigneeName"`
+	ConsigneeAdress      string          `json:"consigneeAdress"`
+	TotalGrossWeight     string          `json:"totalGrossWeight"`
+	HandlingInstructions string          `json:"handlingInstructions"`
+	TotalDimensions      TotalDimensions `json:"totalDimensions"`
+	ShipmentOfPieces     Pieces          `json:"shipmentOfPieces"`
+}
+
+type TotalDimensions struct {
+	Length string `json:"length"`
+	Width  string `json:"width"`
+	Height string `json:"height"`
+	Volume string `json:"volume"`
+}
+
+type Pieces struct {
+	ItemNumber      int    `json:"itemNumber"`
+	ItemDescription string `json:"itemDescription"`
+	Quantity        int    `json:"quantity"`
+	Cartons         string `json:"cartons"`
+	Weight          string `json:"weight"`
+	Unit            string `json:"unit"`
+	HsCode          string `json:"hsCode"`
+	Manufacturer    string `json:"manufacturer"`
 }
